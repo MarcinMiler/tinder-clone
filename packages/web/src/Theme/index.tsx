8eec3fc91bc9794a62b2bd16b51c4479a56c6bb0
Marcin Miler <marcin4951@gmail.com>
2019-02-19 17:46:06 +0100

    Global styling, theme provider

---
+++ packages/web/src/Theme/index.tsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700');
+    
+    body {
+        width: 100%;
+        height: 100vh;
+        margin: 0;
+        font-family: 'Montserrat', sans-serif;
+        box-sizing: border-box;
+    }
+
+    div {
+        box-sizing: border-box;
+    }
+
+    p {
+        margin: 0;
+    }
+`
+
+interface Theme {
+    name: string
+    colors: {
+        [key: string]: string
+    }
+}
+
+export const theme: Theme = {
+    name: 'Deafult',
+    colors: {
+        darkblue: '#21346A',
+        lightgray: '#F1F1F6',
+        darkgray: '#C9C9C9'
+    }
+}
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
+import { GlobalStyle, theme } from './Theme'
 import { Routes } from './Routes'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Routes />, document.getElementById('root'))
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <>
+            <GlobalStyle />
+            <Routes />
+        </>
+    </ThemeProvider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
