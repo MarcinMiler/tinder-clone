658a73ea5342be6b5d5b9a0f2be0fc7ed732a53f
Marcin Miler <marcin4951@gmail.com>
2019-02-25 17:37:01 +0100

    Refactor like tests

--- packages/server/src/modules/like/like.spec.ts
+++ packages/server/src/modules/like/like.spec.ts
@@ -1,23 +1,19 @@
 import * as TypeMoq from 'typemoq'
 import { Repository } from 'typeorm'
 
-import { Match } from '../match/match.entity'
 import { MatchService } from '../match/match.service'
 import { Like } from './like.entity'
 import { LikeService } from './like.service'
 
 describe('Like module', () => {
     let mockLikeRepo: TypeMoq.IMock<Repository<Like>>
-    let mockMatchRepo: TypeMoq.IMock<Repository<Match>>
+    let matchService: TypeMoq.IMock<MatchService>
     let likeService: LikeService
-    let matchService: MatchService
 
     beforeEach(() => {
-        mockMatchRepo = TypeMoq.Mock.ofType<Repository<Match>>()
         mockLikeRepo = TypeMoq.Mock.ofType<Repository<Like>>()
-
-        matchService = new MatchService(mockMatchRepo.object)
-        likeService = new LikeService(mockLikeRepo.object, matchService)
+        matchService = TypeMoq.Mock.ofType<MatchService>()
+        likeService = new LikeService(mockLikeRepo.object, matchService.object)
     })
 
     it('should save like to database and dont create match', async () => {
@@ -32,6 +28,15 @@ describe('Like module', () => {
             .returns(() => null)
             .verifiable()
 
+        matchService
+            .setup(x =>
+                x.createMatch(
+                    TypeMoq.It.isAnyNumber(),
+                    TypeMoq.It.isAnyNumber()
+                )
+            )
+            .verifiable(TypeMoq.Times.never())
+
         mockLikeRepo.setup(x => x.save(TypeMoq.It.isAny())).verifiable()
 
         // act
@@ -39,27 +44,40 @@ describe('Like module', () => {
 
         // assert
         mockLikeRepo.verifyAll()
+        matchService.verifyAll()
     })
 
     it('should create a match and delete like', async () => {
         // arrange
         mockLikeRepo
             .setup(x => x.findOne(TypeMoq.It.isAny()))
-            .returns(
-                async () =>
-                    new Promise(res =>
-                        res({ id: 1, userId: 1, toUserId: 2, date: new Date() })
-                    )
+            .returns(() =>
+                Promise.resolve({
+                    id: 1,
+                    userId: 1,
+                    toUserId: 2,
+                    date: new Date()
+                })
             )
             .verifiable()
 
         mockLikeRepo.setup(x => x.remove(TypeMoq.It.isAny())).verifiable()
 
+        matchService
+            .setup(x =>
+                x.createMatch(
+                    TypeMoq.It.isAnyNumber(),
+                    TypeMoq.It.isAnyNumber()
+                )
+            )
+            .verifiable()
+
         // act
         const like = await likeService.like({ userId: 1, toUserId: 2 })
 
         // arrange
-        mockLikeRepo.verifyAll()
         expect(like).toBe('match')
+        mockLikeRepo.verifyAll()
+        matchService.verifyAll()
     })
 })
