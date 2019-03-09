6dbe0435b0e6c9e3e318090db3d6d5e3d477f843
Marcin Miler <marcin4951@gmail.com>
2019-03-09 18:57:38 +0100

    Login page UI

--- packages/web/src/Pages/Home/index.tsx
+++ packages/web/src/Pages/Home/index.tsx
@@ -1,3 +1,35 @@
 import * as React from 'react'
 
-export const Home = () => <div>Home page</div>
+import { Input, Button } from '@tinder/components'
+import { Background, Wraper, Logo, StyledLink, Center } from './style'
+
+export const Home = () => (
+    <Background>
+        <Wraper>
+            <Logo />
+            <div>
+                <Input
+                    onChange={() => null}
+                    value=""
+                    placeholder="mail@some.com"
+                    label="Email"
+                    type="text"
+                />
+
+                <Input
+                    onChange={() => null}
+                    value=""
+                    label="Password"
+                    type="password"
+                />
+            </div>
+
+            <div>
+                <Button>Login</Button>
+                <Center>
+                    <StyledLink to="/register">Register</StyledLink>
+                </Center>
+            </div>
+        </Wraper>
+    </Background>
+)
